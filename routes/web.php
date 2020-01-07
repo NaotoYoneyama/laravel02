4cd2d58972d48a6f7f7e200595f7f842ad9d6600
EC2 Default User <ec2-user@ip-172-31-35-142.us-east-2.compute.internal>
2020-01-07 11:27:26 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(['prefix' => 'admin','middleware' =>'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('profile/create', 'Admin\profileController@add');
+     Route::post('profile/create', 'Admin\profileController@create'); # 追記
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('profile/edit', 'Admin\profileController@add');
+     Route::post('profile/edit', 'Admin\profileController@create'); # 追記
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
